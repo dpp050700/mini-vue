2cd791bbf3b115a0a5b1782bb7db765759a5c685
戴鹏鹏 <16602124116@163.com>
2022-07-19 22:37:11 +0800

    commit

--- packages/reactivity/src/computed.ts
+++ packages/reactivity/src/computed.ts
@@ -16,6 +16,9 @@ export function computed(getterOptions) {
   return new ComputedRefImpl(getter, setter)
 }
 
+/**
+ * 默认初始化时 _dirty 被设为 true ，在 getter 方法中表示开关打开，需要计算一遍computed 的值，然后关闭开关，之后再获取 computed 的值时由于 _dirty 是 false 就不会重新计算。这就是 computed 缓存值的实现原理
+ */
 class ComputedRefImpl {
   private _value
   private _dirty = true 
